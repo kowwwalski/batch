047093f20300892f065cbd93a66555e1a837e724
kowwwalski <flazh@yandex.ru>
2016-06-24 18:35:50 +0300

    description upd

--- spool.bat
+++ spool.bat
@@ -3,4 +3,4 @@ net stop spooler
 del /f /s /q %systemroot%\System32\spool\PRINTERS\"."
 del /f /s /q %systemroot%\System32\spool\Drivers\w32x86\"."
 net start spooler
-REM запускайте эту шл€пу из под админки, господа
+REM this one requires administrative privileges
