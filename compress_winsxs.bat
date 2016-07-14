071cf12ae1162166af1ea02276ac6d2576c9d643
kowwwalski <flazh@yandex.ru>
2016-07-14 15:16:13 +0400

    winsxs compress added

---
+++ compress_winsxs.bat
@@ -0,0 +1,17 @@
+﻿REM takeown /A /R /F C:\Windows\winsxs\*
+REM icacls C:\Windows\winsxs\* /T /grant Administrators:(F)
+
+@echo off
+REM Включаем раскрытие переменных окружения в момент обращения к ним
+setlocal enabledelayedexpansion
+REM Рекурсивно перебираем все файлы в каталоге C:\Windows\winsxs
+for /R "C:\Windows\winsxs" %%i in (*) do (
+REM Ищем однострочный вывод fsutil
+fsutil hardlink list %%i | find /c /v "" | find /I "1" >nul
+REM Если ссылка единичная, сжимаем файл
+IF !ERRORLEVEL!==0 Compact /C /A /I %%i
+)
+@echo FINISH
+pause>null
+
+icacls C:\Windows\winsxs\* /T /L /grant:r Administrators:(RX)
