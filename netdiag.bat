8275498a8514fd50a74daedfce76497b49e964be
Maxim Chinenkof <flazh@yandex.ru>
2017-09-03 18:57:54 +0300

    Add files via upload

---
+++ netdiag.bat
@@ -0,0 +1,11 @@
+@echo off
+::Diagnose-automata
+date /t > diag-log.txt
+time /t >> diag-log.txt
+set folder=%cd%
+set /p host=Enter hostname or IP-address:
+ping %host% >> .\diag-log.txt
+tracert %host% >> .\diag-log.txt
+ipconfig /all >> .\diag-log.txt
+route print >> .\diag-log.txt
+echo Diagnostic result has been saved in %folder%\diag-log.txt
