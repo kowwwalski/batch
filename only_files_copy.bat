0c6381576b111ddf779052badc30a5c21b2fe086
Maxim Chinenkof <flazh@yandex.ru>
2018-07-14 10:42:29 +0300

    Update only_files_copy.bat

--- only_files_copy.bat
+++ only_files_copy.bat
@@ -1,3 +1,4 @@
 REM copy all files with a certain ext from subdirs to 1 dir
+REM input & output dirs must be different
 @echo on
 for /r %root_dir_with_subdirs% %f in (*.ext) do @copy "%f" %output_dir%
