23832d866d5b52ebb77cb74d5bc6404b03aabe53
Maxim Chinenkof <flazh@yandex.ru>
2018-10-24 23:19:39 +0300

    Delete delTMP.bat

--- delTMP.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 855
-del "%userprofile%\AppData\Local\Temp\*.*" /f /s /q
-del "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5\*.*" /f /s /q
-del "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.MSO\*.*" /f /s /q
-del "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.Outlook\*.*" /f /s /q
-del "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\Low\*.*" /f /s /q
