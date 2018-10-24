9958465f548460c9bea6aa16dd3491ebba282fce
Maxim Chinenkof <flazh@yandex.ru>
2018-10-24 23:15:37 +0300

    Delete spool.bat

--- spool.bat
+++
@@ -1,6 +0,0 @@
-echo off
-net stop spooler
-del /f /s /q %systemroot%\System32\spool\PRINTERS\"."
-del /f /s /q %systemroot%\System32\spool\Drivers\w32x86\"."
-net start spooler
-REM this one requires administrative privileges
