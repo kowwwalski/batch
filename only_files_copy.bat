77c762041bcfd58358e6319a9dc78d081e72352e
Maxim Chinenkof <flazh@yandex.ru>
2018-11-12 21:13:02 +0300

    Delete only_files_copy.bat

--- only_files_copy.bat
+++
@@ -1,4 +0,0 @@
-REM copy all files with a certain ext from subdirs to 1 dir
-REM input & output dirs must be different
-@echo on
-for /r %root_dir_with_subdirs% %f in (*.ext) do @copy "%f" %output_dir%
