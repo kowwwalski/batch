dc069394cd9acf962a86d9d94ee4c1bff0b67f43
Maxim Chinenkof <flazh@yandex.ru>
2020-06-26 10:53:00 +0300

    Update reboot.bat

--- reboot.bat
+++ reboot.bat
@@ -1,3 +1,5 @@
-@set /p answer=Reboot this one? (Y/N):
+@echo off
+set /p answer=Reboot this one? (y/n):
+set answer=%answer:Y=y%
 if %answer%==y ( shutdown /r /t 00 ) else ( echo %informative message% )
 cls
